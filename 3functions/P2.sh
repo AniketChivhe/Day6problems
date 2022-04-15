c4de631fa673669648bf5cd490d1d19a0b566e46
AniketChivhe <aniketchivhe@gmail.com>
2022-04-16 04:32:41 +0530

    [aniket]P2P3 done

---
+++ 3functions/P2.sh
@@ -0,0 +1,33 @@
+
+#! /bin/bash -x
+
+ispalindrome()
+{
+orig=$@
+n=$@
+num=0
+while(( $n >= 1 ))
+do
+rem=`echo $n | awk '{print $1%10}'`
+num=$(( ($num * 10) + $rem  ))
+n=`expr $n / 10 `
+done
+
+if [[ $orig == $num ]]
+then
+	echo " $orig is a Palindrome number "
+else 
+	echo "$orig is not a Palindrome number "
+fi
+
+}
+
+#main Script started from here
+
+ispalindrome 55
+
+ispalindrome 7227
+
+ispalindrome 5005
+
+ispalindrome 5724
